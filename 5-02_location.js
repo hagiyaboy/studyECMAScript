2d910b421ccb4dbb031b6bfaec93af0a212babd2
hagiyaboy <h-bishop@outlook.jp>
2022-02-23 02:00:01 +0900

    [EDIT]5-02_location

--- 5-02_location.js
+++ 5-02_location.js
@@ -1,5 +1,33 @@
 "use strict";
 
-document.getElementById("form").select.onchange = function(){
+const lang = document.querySelector("html").lang;
+
+// if(lang === "ja"){
+//     document.querySelector("option[value='5-02_index.html']").selected = true;
+// }
+// else if(lang === "en"){
+//     document.querySelector("option[value='5-02_index-en.html']").selected = true;
+// }
+// else if(lang === "zh"){
+//     document.querySelector("option[value='5-02_index-zh.html']").selected = true;
+// }
+
+// switch文で書き換え
+
+switch (lang) {
+    case "ja":
+        document.querySelector("option[value='5-02_index.html']").selected = true;
+        break;
+
+    case "en":
+        document.querySelector("option[value='5-02_index-en.html']").selected = true;
+        break;
+
+    case "zh":
+        document.querySelector("option[value='5-02_index-zh.html']").selected = true;
+        break;
+}
+
+document.getElementById("form").select.onchange = function () {
     location.href = document.getElementById("form").select.value;
 }
