0cb913ed4a8e2cea1825bc524d9fe6c6c250c182
hagiyaboy <h-bishop@outlook.jp>
2022-02-23 23:23:00 +0900

    [EDIT]5-03_cookie

--- 5-03_cookie.js
+++ 5-03_cookie.js
@@ -1,17 +1,22 @@
 "use strict";
 
 const agree = Cookies.get("cookie-agree");
-if(agree === "yes"){
-    console.log("クッキーを確認しました");
+const panel = document.getElementById("privacy-panel");
+
+if (agree === "yes") {
+    // console.log("クッキーを確認しました");
+    document.body.removeChild(panel);
+
 }
-else{
-    console.log("クッキーを確認できません")
-    document.getElementById("agreebtn").onclick = function(){
-        Cookies.set("cookie-agree", "yes", {expires: 7});
+else {
+    // console.log("クッキーを確認できません")
+    document.getElementById("agreebtn").onclick = function () {
+        Cookies.set("cookie-agree", "yes", { expires: 7 });
+        document.body.removeChild(panel);
     };
 }
 
 // クッキー削除(テスト用)
-document.getElementById("testbtn").onclick = function(){
+document.getElementById("testbtn").onclick = function () {
     Cookies.remove("cookie-agree");
 };
